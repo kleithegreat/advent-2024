5c7f79cebb0844b54237da5dd80762b1c3b577d1
kevin <kevvinlei89@gmail.com>
2024-12-06 00:31:07 -0600

    day 5 started

--- 5/Main.hs
+++ 5/Main.hs
@@ -1,7 +1,25 @@
-parseInput :: String -> IO[String]
-parseInput = undefined
+import Data.List.Split (splitOn)
 
+type Rule = (Int, Int)
+type Update = [Int]
+
+parseInput :: String -> ([Rule], [Update])
+parseInput input = (rules, updates)
+  where
+    [rulesStr, updatesStr] = splitOn "\n\n" $ filter (/= '\r') input
+    rules = map parseRule $ lines rulesStr
+    updates = map parseUpdate $ lines updatesStr
+
+parseRule :: String -> Rule
+parseRule s = (read x, read y)
+  where
+    [x,y] = splitOn "|" s
+
+parseUpdate :: String -> Update
+parseUpdate = map read . splitOn ","
 
 main :: IO()
 main = do
-    print "lmao"
+    content <- readFile "testInput.txt"
+    let (rules, updates) = parseInput content
+    print rules
