3831bb3619726c44a899a40b6a1bd5383c3190b0
kevin <kevvinlei89@gmail.com>
2024-12-06 14:52:40 -0600

    day 6 edits

--- 6/Main.hs
+++ 6/Main.hs
@@ -1,7 +1,22 @@
-parseInput :: String -> [String]
-parseInput = undefined
+import Data.List (elem, elemIndex)
+
+type Grid = [String]
+
+parseInput :: String -> Grid
+parseInput = lines
+
+findGuard :: Grid -> (Int, Int)
+findGuard [] = (0, 0)
+findGuard (x:xs)
+    | '^' `elem` x = let
+        Just j = elemIndex '^' x
+        in (0, j)
+    | otherwise = let
+        (a, b) = findGuard xs
+        in (1 + a, b)
 
 main :: IO()
 main = do
     contents <- readFile "testInput.txt"
-    print $ parseInput contents
+    let g = lines contents
+    print $ findGuard g
