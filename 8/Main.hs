7cdc7a7e9e84bb9acde66cd29e9e915e844bf5c6
kevin <kevvinlei89@gmail.com>
2024-12-13 15:54:21 -0600

    day 8 started

--- 8/Main.hs
+++ 8/Main.hs
@@ -1,3 +1,15 @@
+import qualified Data.Set as Set
+import Data.Char (isAlphaNum)
+
+type Position = (Int, Int)
+
+getFreqs :: String -> Set.Set Char
+getFreqs = Set.fromList . filter isAlphaNum
+
+solve1 :: [String] -> Int
+solve1 = undefined
+
 main :: IO()
 main = do
-    print "lmao"
+    input <- readFile "testInput.txt"
+    print $ lines input
